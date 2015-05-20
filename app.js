75fb2b8b0ad29880edd1ebf691f377d8f9034bf6
Cristina Swearingen <criss.foltea@yahoo.com>
2015-05-20 12:02:07 -0600

    function for the click event

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ var input=document.getElementById("input");
 var button=document.getElementById("add_todo");
 var newField="";
 
+function newList(){
+  if (input.value.trim().length > 0){
+    // var now = moment();
+  newField = "<li>" + input.value +"</li>";
+  ul.innerHTML += newField;
+  input.value="";
+}}
+
+button.addEventListener("click", function(e){
+   newList(e);
+});
 
 body.addEventListener("click", function(event) {
  if (event.target.tagName.toLowerCase() === "li") {
