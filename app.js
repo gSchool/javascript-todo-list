3519e4a038bfd779079172c73fcdeb41f2b676a7
Matthew Herzl <matthew.herzl@colorado.edu>
2015-05-20 11:06:31 -0600

    moved completed event listeners to own function

--- app.js
+++ app.js
@@ -13,25 +13,17 @@ var listHTML = function(item){
   }
 };
 
-/*
 var setListListeners = function() {
-  var listItems = document.getElementsByTagName('li');
-  listListeners = [];
-  for(var i = 0; i < listItems.length; ++i){
-    listListeners[i].addEventListener('click',function(e){
-      alert('list item was clicked');
-    });
-  }
-};
-*/
-
-document.getElementById('add_todo').addEventListener('click',function(e){
-  list.innerHTML += listHTML(input.value);
-  input.value="";
   var liList = document.getElementsByTagName('li');
   for(var i = 0; i < liList.length; ++i){
     liList[i].addEventListener('click',function(e){
       this.classList.toggle('completed');
     });
   }
+};
+
+document.getElementById('add_todo').addEventListener('click',function(e){
+  list.innerHTML += listHTML(input.value);
+  input.value="";
+  setListListeners();
 });
