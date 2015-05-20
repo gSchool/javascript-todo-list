c131c947f9543c0fa3cee502e1a7d8eb85a95ebe
Eugene Skinner <eskinfrn@gmail.com>
2015-05-20 11:26:31 -0600

    added moment.js functions to app.js. Got the time stamps to function properly with the posts

--- moment.js
+++ moment.js
@@ -3109,27 +3109,3 @@
     return _moment;
 
 }));
-
-var text = document.getElementById("textbox");
-var listarea = document.getElementById("lister");
-var button = document.getElementById("add_todo");
-var tasks = document.getElementsByTagName("li");
-
-var addToDo = function(t){
-  return moment().format() +"<li>" + t + "</li>";
-}
-
-button.addEventListener('click', function(){
-  listarea.innerHTML += addToDo(text.value);
-  text.value = "";
-  initTaskListener();
-})
-
-var initTaskListener = function(){
-  for(var i = 0; i < tasks.length; i++){
-    tasks[i].addEventListener('click',function(e){
-      var task = e.target;
-      task.className = "completed";
-    })
-  }
-}
