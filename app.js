292c8315521921c47af3c34e91a40c5c2e6d5238
Cristina Swearingen <criss.foltea@yahoo.com>
2015-05-20 11:59:49 -0600

    add the change class function

--- app.js
+++ app.js
@@ -2,16 +2,10 @@ var ul=document.querySelector("ul");
 var input=document.getElementById("input");
 var button=document.getElementById("add_todo");
 var newField="";
-button.addEventListener("click", function(e){
-  newField = "<li>" + input.value + "</li>";
-  ul.innerHTML += newField;
-  input.value="";
-});
 
-input.addEventListener("keydown", function(e){
-  if (e.keyCode===13){
-    newField = "<li>" + input.value + "</li>";
-    ul.innerHTML += newField;
-    input.value="";
-  }
-})
+
+body.addEventListener("click", function(event) {
+ if (event.target.tagName.toLowerCase() === "li") {
+    event.target.classList.toggle("completed");
+ }
+ })
