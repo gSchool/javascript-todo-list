d7ccf44c6acb29747aa2dbdac9dc5827f55549e1
Conner Alegre <Conner.Alegre@gmail.com>
2015-05-20 10:34:53 -0600

    Stretch goal, strike out completed on click added

--- app.js
+++ app.js
@@ -4,11 +4,21 @@ var input = document.getElementsByTagName("input")[0];
 
 input.addEventListener('keypress', function() {
     if (event.keyCode == 13) {
-        ul.innerHTML += "<li>" + input.value + "</li>";
+        ul.innerHTML += "<li class='not_completed'>" + input.value + "</li>";
         input.value = "";
     }
 });
 
 addTodo.addEventListener('click', function() {
-    ul.innerHTML += "<li>" + input.value + "</li>";
+    ul.innerHTML += "<li class='not_completed'>" + input.value + "</li>";
+    input.value = "";
 });
+
+document.addEventListener('click', function(event) {
+  var target = event.target;
+  if (target.className === "not_completed") {
+    target.className = "completed"; 
+  } else if (target.className === "completed") {
+    target.className = "not_completed";
+  }
+}, false);
