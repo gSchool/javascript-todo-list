47f245b988b0fc7a0557f31f71978de6aa260eb7
Cristina Swearingen <criss.foltea@yahoo.com>
2015-05-20 11:34:19 -0600

    last changes

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 var ul=document.querySelector("ul");
 var input=document.getElementById("input");
 var button=document.getElementById("add_todo");
-var newField="";
-button.addEventListener("click", function(e){
-  newField = "<li>" + input.value + "</li>";
+var body=document.querySelector("body")
+// var li=document.getElementsByTagName("li");
+
+function newList(){
+  if (input.value.trim().length > 0){
+    // var now = moment();
+  var newField = "<li>" + input.value + now +"</li>";
   ul.innerHTML += newField;
   input.value="";
+}}
+
+
+button.addEventListener("click", function(e){
+   newList(e);
 });
 
 input.addEventListener("keydown", function(e){
   if (e.keyCode===13){
-    newField = "<li>" + input.value + "</li>";
-    ul.innerHTML += newField;
-    input.value="";
+    newList();
   }
 })
+
+body.addEventListener("click", function(event) {
+ if (event.target.tagName.toLowerCase() === "li") {
+    event.target.classList.toggle("completed");
+ }
+ })
