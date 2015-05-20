78329a3283268bb2fe431f61a533cbad3cce06ae
Alan Tweedie <aetweedie@gmail.com>
2015-05-20 11:33:22 -0600

    My solution to the assessment

---
+++ app.js
@@ -0,0 +1,16 @@
+var button = document.getElementById('add_todo');
+var input = document.getElementById('newEvent');
+var output = document.getElementById('todoEvents');
+var completed = document.getElementsByClassName('completed');
+
+button.addEventListener('click', function(){
+  var addEvent = input.value;
+    output.innerHTML = todoEvents.innerHTML + '<ul>' + addEvent;
+  	input.value = '';
+});
+
+output.addEventListener('click', function(){
+  var markComplete = output.value;
+  //need to change the class of <li> to completed//
+
+});
