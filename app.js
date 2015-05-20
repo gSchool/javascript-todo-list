2f7ae9194d62516cbfecd827d0e088c584c7f3fe
silasmartinez <silasm@gmail.com>
2015-05-20 10:38:27 -0600

    Removed useless function

--- app.js
+++ app.js
@@ -1,11 +1,4 @@
 
-
-// Borrowed this function from SO. Thanks stack overflow....
-var capitalizeFirstLetter = function(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-
 var button = document.getElementById('add_todo'),
  input = document.getElementsByTagName('input')[0],
  output=document.getElementsByTagName('ul')[0];
