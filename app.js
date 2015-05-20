dbc6e622cffd497a85ffe82c83908f419cac5328
Jenny Knuth <inkboco@gmail.com>
2015-05-20 11:19:24 -0600

    basic functions are working

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var input = document.getElementById('input');
-var list= document.getElementsByTagName('ul');
+var list= document.getElementById('todoList');
 var button = document.getElementById('add_todo');
 
 button.addEventListener('click', function(){
-console.log(input.value);
-console.log(list);
-    list.innerHTML = "<li>" + input.value + "<li>";
+console.log(list.innerText);
+console.dir(list);
+    list.innerHTML += "<li>" + input.value + "</li>";
+    input.value = "";
 });
