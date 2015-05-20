a049401aba9607273f3ebb40f80eb5d297ee6d20
Tara Gee <geetar40@gmail.com>
2015-05-20 11:44:12 -0600

    the starts

---
+++ app.js
@@ -0,0 +1,10 @@
+function addNewItem(){
+var ListItem = document.createElement('li')
+listItem.innerText = "hello";
+
+  document.getElementsByTagName('ul');
+  list.appendChild(listItem);
+}
+
+var newItem = document.getElementById('add_todo');
+  add_todo.onclick = addNewItem;
