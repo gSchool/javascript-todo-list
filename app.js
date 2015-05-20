3e5117ef6bdb9b9509b10207f7329ddabdea9b13
Matthew Herzl <matthew.herzl@colorado.edu>
2015-05-20 11:04:44 -0600

    now toggles whether completed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 var input = document.getElementsByTagName('input')[0];
 var list = document.getElementsByTagName('ul')[0];
 
-var listItem = function(item){
+var listHTML = function(item){
   if(item === ""){
     return "";
   }
@@ -13,8 +13,25 @@ var listItem = function(item){
   }
 };
 
-document.getElementById('add_todo')
-    .addEventListener('click',function(e){
-  list.innerHTML += listItem(input.value);
+/*
+var setListListeners = function() {
+  var listItems = document.getElementsByTagName('li');
+  listListeners = [];
+  for(var i = 0; i < listItems.length; ++i){
+    listListeners[i].addEventListener('click',function(e){
+      alert('list item was clicked');
+    });
+  }
+};
+*/
+
+document.getElementById('add_todo').addEventListener('click',function(e){
+  list.innerHTML += listHTML(input.value);
   input.value="";
+  var liList = document.getElementsByTagName('li');
+  for(var i = 0; i < liList.length; ++i){
+    liList[i].addEventListener('click',function(e){
+      this.classList.toggle('completed');
+    });
+  }
 });
