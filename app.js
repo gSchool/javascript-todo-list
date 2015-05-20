cf06c767f85dd4be79801bad045e723c2a2eacb5
Eugene Skinner <eskinfrn@gmail.com>
2015-05-20 10:56:59 -0600

    added completed functionality

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var text = document.getElementById("textbox");
 var listarea = document.getElementById("lister");
 var button = document.getElementById("add_todo");
+var tasks = document.getElementsByTagName("li");
 
 var addToDo = function(t){
   return "<li>" + t + "</li>";
@@ -9,4 +10,14 @@ var addToDo = function(t){
 button.addEventListener('click', function(){
   listarea.innerHTML += addToDo(text.value);
   text.value = "";
+  initTaskListener();
 })
+
+var initTaskListener = function(){
+  for(var i = 0; i < tasks.length; i++){
+    tasks[i].addEventListener('click',function(e){
+      var task = e.target;
+      task.className = "completed";
+    })
+  }
+}
