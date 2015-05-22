91bf8fe1ea9c46a6c3c85b219152d058c7e1ac23
Jenny Knuth <inkboco@gmail.com>
2015-05-22 15:33:28 -0600

    working to get the completed class in

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 var input = document.getElementById('input');
 var list= document.getElementById('todoList');
 var button = document.getElementById('add_todo');
+var listItem = document.getElementsByTagName('li');
 
-button.addEventListener('click', function(){
-console.log(list.innerText);
-console.dir(list);
+console.dir(listItem);
+
+var handleEvent = function(event){
+  if(event.keyCode === 13 || event.type === 'click'){
     list.innerHTML += "<li>" + input.value + "</li>";
     input.value = "";
-});
+  }
+};
 
-//input.addEventListener('keyup', function(){
-//console.log(list.innerText);
-//console.dir(list);
-//    list.innerHTML += "<li>" + input.value + "</li>";
-//    input.value = "";
-//});
+button.addEventListener('click', handleEvent);
+input.addEventListener('keydown', handleEvent);
+
+listItem.addEventListener('click', function(event){
+  console.log(event.target);
+  event.target.className = 'completed';
+});
