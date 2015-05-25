47eb79e408adeac0088e62d4dba3781d6763f235
Hillary Skye <skyeinc@qwestoffice.net>
2015-05-25 09:50:17 -0600

    Fixed problems and added stretch goals

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-var add_todo=document.getElementById('add_todo');
-var input=document.getElementById('input');
-var output=document.getElementById('output');
+var button = document.getElementById('add_todo');
+var input = document.getElementById('input');
+var ul = document.getElementsByTagName('ul')[0];
+var body = document.querySelector('body');
+var date = new Date();
+date = moment(date).format('MMMM Do YYYY, h:mm:ss a');
 
-add_todo.addEventListener('click', function(){
-  output.innerHTML = '<li>' + output.innerHTML + (input.value) + '</li'>;
-  input.value = ' ';
+var addTodo = function(e) {
+  if (e.keyCode === 13 || e.type === "click") {
+    ul.innerHTML += '<li>' + input.value + '<br>' + date + '</li>';
+    isComplete = false;
+
+    input.value = ' ';
+  }
+};
+
+button.addEventListener('click', addTodo),
+input.addEventListener('keydown', addTodo);
+
+
+body.addEventListener("click", function(event) {
+  if (event.target.tagName.toLowerCase() === "li") {
+     event.target.classList.toggle("completed");
+   }
 });
