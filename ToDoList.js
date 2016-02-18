2036d14cb97d694435b9029c39a98255ee5602a4
BenAychh <ben.tomas.hernandez@gmail.com>
2016-02-18 12:18:27 -0700

    Now with OOP goodness

---
+++ ToDoList.js
@@ -0,0 +1,12 @@
+function ToDoList(id, name) {
+  var currentID = 0;
+  this.id = id;
+  this.name = name;
+  var tasks = [];
+  this.ul = document.getElementById('listOfTodos');
+  this.addTodo = function (task) {
+    var todo = new ToDo(currentID, task);
+    tasks.push(todo);
+    this.ul.appendChild(todo.getElement());
+  };
+}
