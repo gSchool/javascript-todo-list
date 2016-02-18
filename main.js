8ca95226d2890041835e4dd0c135de52cc2716ba
Dennis Bailey <bailey.dennis@gmail.com>
2016-02-18 08:28:47 -0700

    Now clears the input form field after adding a todo. Created a function to add new todos to the DOM. Added a keypress event to add new todos

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 $(document).ready(function(){
   console.log('Hello!');
   
-  $('#add_todo').on('click', function(){
-    var todo = $(this).val();
-    $('.todo').append('<li>' + todo + '</li>')
+  $('#add_todo').on('click', function(e){
+    e.preventDefault();
+    console.log('test');
+    appendTodo();
+  });
+  
+  $('#input').keypress(function(e) {
+    if(e.which === 13) {
+      e.preventDefault();
+      appendTodo();  
+    }
   });
   
 });
+
+appendTodo = function () {
+  // Create a variable continaing the new todo
+  var newTodo = $('#input').val();
+
+  // Append the new todo to the list of todos and clear out the input form
+  $('#todo').append('<li>' + newTodo + '</li>');
+  $('#input').val('');
+}
