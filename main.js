93c010a1d4ae1cfea02b8bef0f30f2623c9cd9b7
Louisa Reese <louisa.e.reese@gmail.com>
2016-02-17 20:39:24 -0700

    completed todo app successfully

---
+++ main.js
@@ -0,0 +1,18 @@
+// when completed, add class("completed")
+
+$(document).ready(function(){
+	console.log("thundercats go");
+});
+
+$("form").on("submit", function(event){
+	event.preventDefault();
+
+	var task = $("#task").val();
+	console.log(task);
+	$("ul").append("<li>"+ task +"</li>");
+	$("#task").val("");
+});
+
+$("ul").on("click", "li", function(){
+	$(this).addClass("completed");
+});
