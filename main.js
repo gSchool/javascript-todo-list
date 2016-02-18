614273bc99c3cafb692145d9a81653953d3ea206
Jon Hammond <jonh1016@gmail.com>
2016-02-18 15:48:39 -0700

    Added function that allows user to add item to the list when pressing enter/return

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
 $(document).on('ready', function() {
+  addToDo();
+  pressEnter();
+});
 
-})
+var userInput;
 
-var userInput =
+function addToDo (event) {
+  $('#add_todo').on('click', function() {
+    userInput = $('#userInput').val();
+    $('#todoList').append("<li>" + userInput + "</li>");
+  });
+}
+
+function pressEnter () {
+  $('#userInput').keypress(function(event) {
+    if(event.which === 13) {
+      event.preventDefault();
+      userInput = $('#userInput').val();
+      $('#todoList').append("<li>" + userInput + "</li>");
+    }
+  });
+}
+
