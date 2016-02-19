5a05ecb731d9452115707f02e145f0d89d4f0174
Valerie Kraucunas <valeriekraucunas@gmail.com>
2016-02-18 22:45:53 -0700

    oop'd work

--- main.js
+++ main.js
@@ -1,15 +1,35 @@
-var newList = require('./oop').List;
-var newTask = require('./oop').Task;
+$(document).ready(function() {
 
-$('#add_todo').on('click', function() {
-    var userInput = $('input').val();
-    $('ul').append('<li>'+userInput+'</li>');
-    $('input').val('').focus();
+    var list1 = new List ({
+        listName: "Today's todo",
+        tasks: [ {id: 2001, name: "Walk the pups", completed: false}]
+    });
+
+    $('#add_todo').on('click', function() {
+        var userInput = $('input').val();
+        $('ul').append('<li>'+userInput+'</li>');
+        $('input').val('').focus();
+
+        var task = new Task(userInput);
+        list1.addTask(task);
+        console.log(list1);
+    });
+
+
+    $('section').on('click', 'li', function() {
+        var complete = 'completed';
+        $(this).addClass(complete);
+
+        var name = $(this).text();
+        var taskList = list1.tasks;
+
+        for (var i=0; i < taskList.length; i++) {
+            if (taskList[i].name === name) {
+                taskList[i].completed = true;
+            }
+        }
+    });
+
 
 });
 
-
-$('section').on('click', 'li', function() {
-    var complete = 'completed';
-    $(this).addClass(complete);
-});
--- oop.js
+++ oop.js
@@ -1,31 +1,27 @@
-module.exports = {
-    List: List,
-    Task: Task
-};
-
 var List = function(options) {
-    this.id = Math.floor((Math.random()*100)+1),
+    this.id = Math.floor((Math.random()*1000)+1),
     this.listName = options.listName,
-    this.tasks = options.tasks
+    this.tasks = options.tasks,
+    this.addTask = function(taskObj) {
+        this.tasks.push(taskObj);
+    };
 };
 
-List.prototype.addTask = function(taskObj) {
-    this.tasks.push(taskObj);
+
+var Task = function(name) {
+    this.id = Math.floor((Math.random()*2000)+1000),
+    this.name = name,
+    this.completed = false
+    // this.done = function() {
+    //     this.completed = true;
+    // };
 };
 
+//tests
 // var newList = new List ({
 //     listName: "todo",
 //     tasks: []
 // });
-
-
-
-var Task = function(name) {
-    this.id = Math.floor((Math.random()*200)+100),
-    this.name = name,
-    this.completed = false
-};
-
 // var newTask = new Task("walk the pups");
 // var newTask2 = new Task("get milk");
 
