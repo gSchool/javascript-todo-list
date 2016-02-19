2a10d43c49e5904099c5718d02585f83d1ec4f17
Daniel Klein <danhirschklein@gmail.com>
2016-02-18 23:53:48 -0700

    Created Oop constructors but havent linked them into the appendange yet

--- js/app.js
+++ js/app.js
@@ -2,6 +2,12 @@ $('#submit').on('click', function() { var task = $('#text').val();
   $('ul').append('<li>' + task + '</li>')
 })
 
+var ToDo = function (task){
+  this.id = 0;
+  this.task = task;
+  this.completed = false;
+}
+
 var ToDoList = function (ln) {
   this.id = 0;
   this.listName = ln;
