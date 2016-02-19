9f03a8791be2e878ba3f4e4f0912b4a4b9ce0542
Danny Robinson <daniel@djrobinson.me>
2016-02-18 21:37:04 -0700

    Completed OOP changes

--- app.js
+++ app.js
@@ -1,18 +1,70 @@
+document.getElementById('todo').addEventListener('click', addTodo);
+document.getElementById('todont').addEventListener('click', addTodo);
 
-document.getElementById('add_todo').addEventListener('click', addTodo);
+var idCounter = 0;
+
+
+//////////////DATA SETUP///////////////////
+var lists = [];
+function List(id, name){
+  this.id = id;
+  this.listName = name;
+  this.tasks = [];
+}
+
+var todos = [];
+function Todo(id, task){
+  this.id = id;
+  this.task = task;
+  this.completed = false;
+}
+
+var todoList = new List(1, 'todo');
+var todontList = new List(2, 'todont');
+
+lists.push(todoList, todontList);
+
+
+//////////////APPLICATION LOGIC///////////////////
 
 function addTodo(){
+  //get id from button pushed for comparison
+  var whichList = this.id
   var todo = document.getElementById('input_todo').value;
-  var list = document.getElementById('todo_list');
-  var listItem = document.createElement("li");
-  var node = document.createTextNode(todo);
-  listItem.appendChild(node);
-  list.appendChild(listItem);
-  addListeners();
-  document.getElementById('input_todo').value = "";
+
+  //if there's something in da box, determine which list it belongs to
+  if (todo.trim()){
+    lists.forEach(function(list){
+      if (list.listName === whichList) list.tasks.push(idCounter);
+    });
+
+    //create the new todo
+    var todoObj = new Todo(idCounter, todo);
+    todos.push(todoObj);
+
+    //create the list item to pass to appendTodo funk
+    var listItem = document.createElement('li');
+    var node = document.createTextNode(todo);
+    listItem.appendChild(node);
+    listItem.className = whichList;
+    listItem.setAttribute('id', idCounter);
+
+    appendTodo(listItem);
+    idCounter++;
+  }
 }
 
 
+//appends to the ul on the page
+function appendTodo(listItem){
+    var list = document.getElementById('todo_list');
+    list.appendChild(listItem);
+    addListeners();
+    document.getElementById('input_todo').value = '';
+}
+
+
+//runs through each li to add listener
 function addListeners(){
   var todos = document.getElementsByTagName('li')
   console.log(typeof(todos));
@@ -21,6 +73,9 @@ function addListeners(){
   }
 }
 
+//sets object to complete & adds class to the DOM
 function completeTodo(){
+  var id = parseInt(this.id);
+  todos[id].completed = true;
   this.className = "completed";
 }
