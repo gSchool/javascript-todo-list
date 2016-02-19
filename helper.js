4c4053978182f2e66f3d9ebd37ceaec100dfe892
Dennis Bailey <bailey.dennis@gmail.com>
2016-02-19 08:48:51 -0700

    Click and return adding of tasks now creates objects

--- helper.js
+++ helper.js
@@ -1,46 +1,47 @@
-var listsCounter = 0;
-var tasksCounter = 0;
-
-
-var Tasks = function() [];
-
-Tasks.prototype.newTodo = {
-  this.id = tasksCounter,
-  this.task = $('#input').val(),
-  this.completed = no,
-};
-
 //////////////////////
 var listsCounter = 0;
 var tasksCounter = 0;
 
 var todoLists = [];
 
+// List Constructor
 var TodoList = function (name) {
-  this.listID = listsCounter,
+  this.listID = 'list-' + listsCounter,
   this.listName = name,
   this.tasks = [];
 };
 
+// Create the inital list
+todoLists.push(new TodoList('todo'));
+listsCounter += 1;
+
+// Task Constructor
 var Task = function (todo) {
-  this.taskID = tasksCounter,
+  this.taskID = 'task-' + tasksCounter,
   this.task = todo,
   this.completed = 'false';
 };
 
-todoLists.push(new TodoList('list0'));
-listsCounter += 1;
-todoLists.push(new TodoList('list1'));
+var addTask = function(listIndex, newTodo) {
+    var task = new Task(newTodo);
+    todoLists[listIndex].tasks.push(task);
+    tasksCounter += 1;
+};
 
-var newTodo = new Task('what am I doing?');
-console.log(newTodo);
-todoLists[0].tasks.push(newTodo); 
 
-tasksCounter += 1;
+// todoLists.push(new TodoList('list0'));
+// listsCounter += 1;
+// todoLists.push(new TodoList('list1'));
 
-var newTodo = new Task('How am I doing?');
-console.log(newTodo);
-todoLists[0].tasks.push(newTodo);
-
-console.log(todoLists);
-console.log(todoLists[0].tasks[0]);
+// var newTodo = new Task('what am I doing?');
+// console.log(newTodo);
+// todoLists[0].tasks.push(newTodo); 
+// 
+// tasksCounter += 1;
+// 
+// var newTodo = new Task('How am I doing?');
+// console.log(newTodo);
+// todoLists[0].tasks.push(newTodo);
+// 
+// console.log(todoLists);
+// console.log(todoLists[0].tasks[0]);
--- main.js
+++ main.js
@@ -4,34 +4,42 @@ $(document).ready(function(){
   // Add new todos to the DOM if the 'add todo' button is clicked
   $('#add_todo').on('click', function(e){
     e.preventDefault();
-    appendTodo();
+    
+    // Create a variable continaing the new todo
+    var newTodo = $('#input').val();
+    
+    // Update objects
+    var update = addTask(0, newTodo)
+    appendTodo(newTodo);
   });
   
   // Add new todos to the DOM if the return key is used in the input field
   $('#input').keypress(function(e) {
+    var newTodo = $('#input').val();
+    
     if(e.which === 13) {
       e.preventDefault();
-      appendTodo();  
+      // Update objects
+      var update = addTask(0, newTodo)
+      appendTodo(newTodo);  
     }
   });
   
   // Any todo items that are clicked get the completed class
-  $('#todo').on('click', 'li', function() {
+  $('#todo-01').on('click', 'li', function() {
     $(this).addClass('completed');
+    
   })
   
   // Any todo items that are completed and get clicked again nolonger appear completed
-  $('#todo').on('click', '.completed', function() {
+  $('#todo-01').on('click', '.completed', function() {
     $(this).removeClass('completed');
   })
   
 });
 
-var appendTodo = function () {
-  // Create a variable continaing the new todo
-  var newTodo = $('#input').val();
-
+var appendTodo = function (newTodo) {
   // Append the new todo to the list of todos and clear out the input form
-  $('#todo').append('<li>' + newTodo + '</li>');
+  $('#todo-01').append('<li>' + newTodo + '</li>');
   $('#input').val('');
 }
