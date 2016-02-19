6cbc0ad7d2ae1188ef0f2df5b4d6b6be19d004d8
millsmcilroy <millsmcilroy@gmail.com>
2016-02-18 22:36:51 -0700

    done

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+//Create the todoList object
+var todoList = {
+  id: "theList",
+  listName: "myList",
+  tasks: []//Stores Todos
+};
+
+//The Todo task constructor
+function Todo(id, task) {
+  this.id = id;
+  this.task = task;
+  this.completed = false;
+}
+
 //If button clicked
 $('#add_todo').on('click', function () {
   append_todo();
@@ -13,7 +27,7 @@ $('input').keypress(function (e) {
 });
 
 //If list item clicked
-$('ul').on('click','li',function(){
+$('#theList').on('click','li',function(){
   crossOut(this);
 });
 
@@ -22,14 +36,36 @@ function append_todo () {
   var $input = $('input');
 
   if ($input.val()) {
+    //Grab the todo's value from the input box
     var val = $input.val();
-    $('ul').append('<li>' + val + '</li>');
-    $input.val("");//Clears input box
+    //Create an id for the todo based on array position
+    var id = "Task" + (todoList.tasks.length + 1);
+    //Create the todo with the Todo constructor
+    var todo = new Todo (id, val);
+    //Add the todo to the todoList array
+    todoList.tasks.push(todo);
+    //Create a jQuery list item
+    var listItem = $('<li>' + val + '</li>');
+    //Give the listItem the todo's id
+    listItem.attr('id', id);
+    //Append to the ul
+    $('#theList').append(listItem);
+    //Clear the input box
+    $input.val("");
   }
 }
 
 //Adds the completed class to the given li so it appears crossed out
 function crossOut(li) {
   $(li).addClass('completed');
+
+  //Mark the todo object completed in the todoList tasks array
+  for (var i = 0; i < todoList.tasks.length; i++) {
+    if (li.id === todoList.tasks[i].id)
+      todoList.tasks[i].completed = true;
+  }
+
+
+
 }
 
