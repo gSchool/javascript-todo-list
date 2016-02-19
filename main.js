e2a5135ee3b9c3205670c7adcc73bd033ba07376
Louisa Reese <louisa.e.reese@gmail.com>
2016-02-19 09:58:49 -0700

    branching while this works, starting to refactor for oop

--- main.js
+++ main.js
@@ -4,15 +4,20 @@ $(document).ready(function(){
 	console.log("thundercats go");
 });
 
-$("form").on("submit", function(event){
-	event.preventDefault();
 
-	var task = $("#task").val();
-	console.log(task);
-	$("ul").append("<li>"+ task +"</li>");
-	$("#task").val("");
-});
 
-$("ul").on("click", "li", function(){
-	$(this).toggleClass("completed");
-});
+
+	$("form").on("submit", function(event){
+		event.preventDefault();
+
+		var task = $("#task").val();
+		console.log(task);
+		$("ul").append("<li>"+ task +"</li>");
+		$("#task").val("");
+	});
+
+
+
+	$("ul").on("click", "li", function(){
+		$(this).toggleClass("completed");
+	});
