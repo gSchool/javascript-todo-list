655ee4a0ba065368ff4fcc71790a98ae221299c9
Louisa Reese <louisa.e.reese@gmail.com>
2016-02-20 13:19:25 -0700

    removed console logs

--- dom.js
+++ dom.js
@@ -6,18 +6,14 @@ $(document).on("ready", function(){
 $("form").on("submit", function(event){
 		event.preventDefault();
 
-		console.log("submitted");
-	console.log( $("#task").val() );
-
 		var task = new todo({
 			id: "task",
 			task: $("#task").val(),
 			completed: false
 		});
 		myTodos.addTask(task);
-console.log("added", myTodos);
+
 		$("ul").append("<li id="+task.id+">"+ task.task +"</li>");
-		console.log(task);
 		// clears input field
 		$("#task").val("");
 	});
@@ -29,5 +25,4 @@ $("ul").on("click", "li", function(){
 		$(this).toggleClass("completed");
 
 		myTodos.completeTask(task);
-		console.log("completed", myTodos);
 	});
