dc803a6fb6f0c9690f30ee8a40ff085f8e5671c7
Kathy Dieser <katdiese@gmail.com>
2016-02-21 19:19:57 -0700

    added prototypes for list and lI

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
+var newList = function(listName) {
+  this.id = "id-" + listName;
+  this.task = listName;
+  this.tasks = [];
+}
+
+var newItem = function(id){
+  this.id = "id-" +id;
+  this.task = id;
+  this.completed = false;
+}
+
 function addOnClick(event) {
   event.preventDefault();
   $('form').on('submit', function() {
-    console.log($('input').val());
+    var thing = $('input').val();
     if($('input').val()) {
       appendAndClear();
     }
